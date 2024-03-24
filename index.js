707a45bd6a5d6fea9e3557e40b2ad5369f6431dc
Mercy Oroo <mercy.oroo@student.moringaschool.com>
2024-03-24 08:11:39 +0300

    javascript functions continued

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ function mondayWork (activity2 = "go to the office") {
     let result2 = `This Monday, I will ${activity2}.`
     return result2 
 }
-// function wrapAdjective (adj = "*") {
-//     let innerFunction = function (description = 'special') {
-//         return `You are ${description} !`
-//         }
-// return innerFunction
-// }
+
 function wrapAdjective (highlight = "*") {
     return function ( adjective = "special") {
       return `You are ${highlight}${adjective}${highlight}!`
